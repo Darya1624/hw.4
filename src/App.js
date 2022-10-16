ed4f9c1a6c49ab4f9845cce88b3daa1b6cd35cc6
Darya <daryaevans16@gmail.com>
2022-10-16 18:07:15 +0600

    add div button

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Button from "./components/Button"
+function App() {
+  return (
+    <div className="App">
+      
+      <h2>UCLA</h2>
+      <p>The undergraduate and graduate degree search page displays information about the program, a major map showing the courses you will need to take each semester, and other special offerings specific to your chosen course of study.
+
+Find the perfect major. UCLA offers more than 250 programs, taught by skilled, student-focused professors. You’ll have the knowledge, experiences and opportunities to deeply explore your interests.
+
+Whether you're an undergraduate student pursuing a bachelor's degree – including 125 undergraduate majors and 90 minors — or a graduate student pursuing a master's or doctoral program — 120 graduate and doctoral programs for you to explore — we can help you find the program that fits you.
+
+For students who are unsure what major they want to pursue, UCLA offers new student sessions. Through these sessions, students are assisted in choosing majors, planning courses, and understanding graduation requirements.
+</p>
+     <Button/>
+   
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+function Button () {
+    return(
+        <button>Send</button>
+    )
+    
+}
+export default Button;
